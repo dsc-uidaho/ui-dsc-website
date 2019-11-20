7f0a6a49c251a573808c4a6a0b7ce640a4746b4d
Koffi, Koffi Anderson <kandersonko@gmail.com>
2019-11-19 19:01:45 -0800

    Add join and mission sections

---
+++ src/components/join-section.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Container from './container';
+
+
+const JoinSection = () => (
+    <div className="flex flex-col items-center justify-center w-screen bg-red-600 text-white py-16">
+        <Container>
+            <h1 className="text-6xl mb-2">
+                How can I Join
+            </h1>
+
+            <div className="flex flex-col">
+                <p className="text-xl">
+                    We meet on for workshops on <b>Monday evenings at 5:00pm</b> in <b>Room 120</b> of the <b>University of Idaho Library.</b> To officially join the club, just talk to a core team member after a meeting!
+                </p>
+            </div>
+        </Container>
+    </div>
+);
+
+export default JoinSection;
---
+++ src/components/mission-section.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Container from './container';
+
+
+const MissionSection = () => (
+    <div className="flex flex-col items-center justify-center py-16 w-screen text-gray-600">
+        <Container>
+            <h1 className="text-6xl text-green-400 mb-3">
+                Mission Statement
+            </h1>
+
+            <div className="flex flex-col py-10">
+                <h3 className="text-green-400 font-bold text-2xl mb-3">
+                    Taking on tech together.
+                </h3>
+                <p className="text-xl">
+                    To cultivate <b>tech-related gatherings</b> in the Moscow community that promote <b>personal growth</b> and positive <b>community impact</b>, so that every member who leaves a gathering does so feeling <b>empowered.</b>
+                </p>
+            </div>
+        </Container>
+    </div>
+);
+
+export default MissionSection;
